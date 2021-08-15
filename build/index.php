f03c8be69db48deae28c022f7ccbe40fb3aa467e
Dmitriy Romanov <romanovua1991@gmail.com>
2021-08-16 00:56:06 +0300

    current changes

--- build/index.php
+++ build/index.php
@@ -1,11 +1,64 @@
 <?php include($_SERVER["DOCUMENT_ROOT"].'/build/components/header.php');?>
 
-<main>
-    <div class="intro">
-        <div class="intro__background">
-            <img src="/src/img/nordwood-themes-kRNZiGKtz48-unsplash.jpg" alt="top background">
+<div class="intro intro--main">
+    <div class="intro__wrapper" style="background-image: url('/build/img/nordwood-themes-kRNZiGKtz48-unsplash.jpg')">
+        <div class="container intro__content">
+            <h1 class="intro__title">
+                <span>АРВ-качественное обслуживание</span>
+                <span>техники и товаров для офиса.</span>
+            </h1>
+            <div class="intro__subtitle txt-md">
+                <span>Оргтехника, сервис, кликовые контракты,</span>
+                <span>пост печатная техника, компьютеры</span>
+            </div>
+            <div class="intro__services txt-lg">
+                <div class="intro__service">
+                    <div class="intro__service-view">
+                        <svg class="">
+                          <use xlink:href="/build/img/sprite.svg#barter"></use>
+                        </svg>
+                    </div>
+                    <div class="intro__service-title">
+                        <span>Оптовые </span>
+                        <a class="intro__service-link" href="#">продажи</a>
+                    </div>
+                </div>
+                <div class="intro__service">
+                    <div class="intro__service-view">
+                        <svg class="">
+                            <use xlink:href="/build/img/sprite.svg#handshake"></use>
+                        </svg>
+                    </div>
+                    <div class="intro__service-title">
+                        <span>Решения для </span>
+                        <a class="intro__service-link" href="#">бизнеса</a>
+                    </div>
+                </div>
+                <div class="intro__service">
+                    <div class="intro__service-view">
+                        <svg class="">
+                            <use xlink:href="/build/img/sprite.svg#settings"></use>
+                        </svg>
+                    </div>
+                    <div class="intro__service-title">
+                        <span>Сервис и </span>
+                        <a class="intro__service-link" href="#">ремонт</a>
+                    </div>
+                </div>
+                <div class="intro__service">
+                    <div class="intro__service-view">
+                        <svg class="">
+                            <use xlink:href="/build/img/sprite.svg#consult"></use>
+                        </svg>
+                    </div>
+                    <div class="intro__service-title">
+                        <span></span>
+                        <a class="intro__service-link" href="#">Услуги</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</main>
+</div>
 
 <? include($_SERVER["DOCUMENT_ROOT"].'/build/components/footer.php');?>
--- src/pages/components/footer.php
+++ src/pages/components/footer.php
@@ -1,11 +1,47 @@
     </main>
 
     <footer class="footer">
-        
+        <div class="footer__top">
+            <div class="footer__background" style="background-image: url('/build/img/mimi-thian-ZKBzlifgkgw-unsplash.jpg')">
+                <div class="container">
+                    <div class="footer__form-box">
+                        <div class="footer__form-design right"></div>
+                        <div class="footer__form-design left"></div>
+                        <div class="footer__form-title h2">Оставьте заявку!</div>
+                        <div class="footer__form-subtitle txt-lg">Наш менеджер ответит на интересующие Вас вопросы в течении 10 минут.</div>
+                        <form class="footer__form" method="post">
+                            <div class="footer__form-item txt-sm">
+                                <input type="text" name="NAME" required="" placeholder="ФИО">
+                            </div>
+                            <div class="footer__form-item txt-sm">
+                                <input type="text" name="COMPANY" required="" placeholder="Компания">
+                            </div>
+                            <div class="footer__form-item txt-sm">
+                                <input type="tel" name="PHONE" required="" placeholder="Номер телефона">
+                            </div>
+                            <div class="footer__form-item txt-md">
+                                <button class="btn btn-primary" type="submit">Заказать звонок</button>
+                            </div>
+                            <div class="footer__form-note">
+                                <label>
+                                    <input type="checkbox" value="">
+                                    <span class="footer__form-note-marker"></span>
+                                    <span>Согласен на обработку персональных данных</span>
+                                </label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__bottom">
+
+        </div>
+
     </footer>
 
 
-    <script src="https://api-maps.yandex.ru/2.1/?apikey=<ваш API-ключ>&lang=ru_RU" type="text/javascript"></script>
+    <?/*<script src="https://api-maps.yandex.ru/2.1/?apikey=<ваш API-ключ>&lang=ru_RU" type="text/javascript"></script>*/?>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="../../build/js/main.min.js"></script>
     <script src="../../build/js/script.js"></script>
